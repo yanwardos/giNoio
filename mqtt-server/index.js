e88a54582999088b02c320c98830b6e62bdc83f2
yanwardos <yanwardos@gmail.com>
2024-08-02 02:54:55 +0700

    added mqtt-server

--- mqtt-server/index.js
+++ mqtt-server/index.js
@@ -1,3 +1,28 @@
+
+let clientId = "igonio-server-client";
+let username = "igonio-server";
+let password = "igonio-server";
+
+const serverClient = mqtt.connect('mqtts://b6de8b3b.ala.asia-southeast1.emqxsl.com:8883', {
+    clientId,
+    username,
+    password,
+    // ...other options
+  });
+
+  let topic_device = 'devices/#';
+  serverClient.subscribe(topic_device, 0, (error)=>{
+    if(error){
+        console.log('Subscribe error: ', error);
+        return;
+    }
+
+    console.log(`Subscribed to topic: '${topic_device}'`);
+  });
+
+
+  
+
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
