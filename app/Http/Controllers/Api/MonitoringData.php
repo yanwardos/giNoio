8c3349b12fa295d94eeb154c1570e9dc684570a3
yanwardos <yanwardos@gmail.com>
2024-08-08 14:05:09 +0700

    update

---
+++ app/Http/Controllers/Api/MonitoringData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MonitoringData extends Controller
+{
+    public function storeData() {
+        
+    }
+}
