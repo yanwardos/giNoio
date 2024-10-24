a54715461473bc9d242f2e9d56fe9fb7f9f21997
yanwardos <yanwardos@gmail.com>
2024-10-24 08:54:21 +0700

    update

--- resources/views/medis/pasien/create.blade.php
+++ resources/views/medis/pasien/create.blade.php
@@ -162,23 +162,7 @@
                     </div>
                 </div>
             </form>
-        </div>
-        <div class="col-12 col-lg-10 col-xl-8 d-flex flex-column p-2">
-            <div class="card bg-gray-light">
-                <div class="card-header">
-                    <div class="card-title"> 
-                        <span class="h6">Riwayat Terapi</span>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <!-- TODO: RIWAYAT -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div> 
     </div><!-- /.container-fluid -->
 @endsection
 
